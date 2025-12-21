b319d6d7194aba73437d915e3db41d1bfd19e451
rhectoroc <rhectoroc@gmail.com>
2025-12-21 09:07:34 -0400

    Refactor Dockerfile for streamlined build process and remove legacy frontend Dockerfile

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,5 +3,7 @@ import type { Config } from '@react-router/dev/config';
 export default {
 	appDirectory: './src/app',
 	ssr: true,
-	prerender: ['/*?'],
+  // Eliminamos el prerenderizado de rutas dinámicas durante el build
+  // para evitar que el compilador intente acceder a carpetas inexistentes
+  prerender: false,
 } satisfies Config;
